a37ea7b4f0cdde62c8152cf2432904861dddfcdc
fsszns@163.com <fsszns@163.com>
2016-05-06 14:49:18 +0800

    5-6-2016

---
+++ div1cf#348a/div1cf#348a/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  div1cf#348a
+//
+//  Created by ChanVain on 16/4/29.
+//  Copyright © 2016年 ChanVain. All rights reserved.
+//
+
+#include <iostream>
+int n,m,q;
+int main(int argc, const char * argv[]) {
+    while(~scanf("%d%d%d",&n,&m,&q))
+          {
+              for(int i=1;i<=q;i++)
+              {
+                  int a;
+                  scanf("%d",&a);
+              }
+          }
+    return 0;
+}
