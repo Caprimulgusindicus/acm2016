7cba4e4b46b326b0c70cdac1c5075ab398be902a
fsszns@163.com <fsszns@163.com>
2016-10-31 20:00:33 +0800

    no message

--- codeforces/hdu5616.cpp
+++ codeforces/hdu5616.cpp
@@ -2,21 +2,43 @@
 #include <cstring>
 #include <iostream>
 using namespace std;
-int a[111];
 int vis[11111];
+int dp[111][2222];
 int main()
 {
 	int T;
 	scanf("%d",&T);
 	while(T--)
 	{
+		memset(dp,0,sizeof(dp));
 		int n;
+		dp[0][0]=1;
+
 		scanf("%d",&n);
 		for(int i=1;i<=n;i++)
-			scanf("%d",&a[i]),vis[a[i]]++;
+		{
+			int kk;
+			scanf("%d",&kk);
+			for(int j=0;j<=2000;j++)
+			{
+				if(!dp[i-1][j]) continue;
+				dp[i][j]=dp[i-1][j];
+				dp[i][j+kk]=1;
+				if(j>kk) dp[i][j-kk]=1;
+				else dp[i][kk-j]=1;
+			}
+		}
+			
 		int m;
 		scanf("%d",&m);
-		
+		for(int i=1;i<=m;i++)
+		{
+			int q;
+			scanf("%d",&q);
+			if(dp[n][q]) cout<<"YES"<<endl;
+			else cout<<"NO"<<endl;
+		}
+
 
 	}
 	return 0;
